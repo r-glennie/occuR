9f1fb10299e4ace656c420a25b9439c8d5ff6843
richard <glennie@glennies.co.uk>
2020-03-12 15:55:04 +0000

    cleaning R scripts

--- R/occu_tmb.R
+++ R/occu_tmb.R
@@ -79,9 +79,9 @@ make_matrices <- function(forms, visit_data, site_data) {
 #' Fit occupancy model 
 #'
 #' @param forms list of formulae for psi (occupancy) and p (detection) 
-#' @param visit_data data.table with row for each visit with a y column for detection record 
-#' @param site_data data.table with row for each site 
-#' @param start list of starting values for beta_psi, beta_p 
+#' @param visit_data data.table with row for each visit with a "obs" column for detection record 
+#' @param site_data data.table with row for each site x occasion
+#' @param start named list of starting values for beta_psi and beta_p - fixed effects parameters
 #' @param print if TRUE then possibly useful info is printed out 
 #' @return fitted occupied model object 
 #' @export
@@ -89,11 +89,12 @@ make_matrices <- function(forms, visit_data, site_data) {
 #' @importFrom TMB MakeADFun sdreport normalize
 #' @useDynLib occu_tmb
 fit_occu <- function(forms, visit_data, site_data, start = NULL, print = TRUE) {
-  
   ## DATA 
   # order data 
   site_data <- site_data[order(site_data$site, site_data$occasion),]
   visit_data <- visit_data[order(visit_data$site, visit_data$occasion, visit_data$visit),]
+  # check data 
+  check_inp(forms, visit_data, site_data, start, print)
   # number of sites
   nsites <- uniqueN(site_data$site)
   # number of occasions 
@@ -133,9 +134,12 @@ fit_occu <- function(forms, visit_data, site_data, start = NULL, print = TRUE) {
   beta_psi <- rep(0, mats$nfix_psi)
   beta_p <- rep(0, mats$nfix_p)
   if (!is.null(start)) {
-    beta_psi <- start$beta_psi 
-    beta_p <- start$beta_p 
+    len <- min(length(start$beta_psi), beta_psi)
+    beta_psi[1:len] <- start$beta_psi[1:len]
+    len <- min(length(start$beta_psi), beta_psi)
+    beta_p[1:len] <- start$beta_p[1:len] 
   }
+  
   # random effects 
   # occupancy
   if (is.null(mats$S_psi)) {
@@ -190,20 +194,91 @@ fit_occu <- function(forms, visit_data, site_data, start = NULL, print = TRUE) {
   
   ## RESULTS
   val <- list(res = res, fit = fit, forms = forms, mats = mats)
-  class(val) <- "occu"
+  class(val) <- "occuR"
   
   return(val)
 
 }
 
-#' Summary of occu object 
+#' Simple input checks for fit_occu
+#'
+#' @inheritParams fit_occu
+#'
+#' @return stops execution if input is invalid 
+check_inp <- function(forms, visit_data, site_data, start, print) {
+  
+  # check forms
+  if (class(forms) != "list") stop("forms must be a named list")
+  if (! (length(forms) == 2 & "p" %in% names(forms) & "psi" %in% names(forms))) {
+    stop("forms must be a named list with at least 'p' and 'psi' elements")
+  }
+  if (class(forms$p) != "formula" | class(forms$psi) != "formula") stop("Invalid formula")
+  
+  # check visit_data 
+  if (!("data.table" %in% class(visit_data))) stop("visit_data should be a data.table")
+  if (!("obs" %in% colnames(visit_data))) stop("visit_data does not have a column named 'obs'")
+  if (!("site" %in% colnames(visit_data))) stop("visit_data does not have a column named 'site'")
+  if (!("occasion" %in% colnames(visit_data))) stop("visit_data does not have a column named 'occasion'")
+  if (!("visit" %in% colnames(visit_data))) stop("visit_data does not have a column named 'visit'")
+  num <- visit_data[, .(max = max(visit), n = uniqueN(visit)), .(site, occasion)]
+  if (any(num$max != num$n)) stop("visit_data has missing visits or visits are mis-numbered: ", num[which(num$max != num$n),])
+  if (any(abs(visit_data$obs) > 1e-10 & abs(visit_data$obs - 1) > 1e-10)) stop("visit_data obs has entries that are not zero or one")
+  
+  # check site data 
+  num <- site_data[, .(max = max(site), n = uniqueN(site)), .(occasion)]
+  if (any(num$max != num$n)) stop("site_data has missing sites or sites are mis-numered: ", num[which(num$max  != num$n,)])
+  num <- site_data[, .(max = max(occasion), n = uniqueN(occasion))]
+  if (any(num$max != num$n)) stop("site_data has missing occasions or occasions are mis-numered")
+  
+  # consistency between visit_data and site_data
+  if (!all(visit_data$site %in% site_data$site)) stop("visit_data has sites not included in site_data")
+  siteocc <- paste0(visit_data$site, visit_data$occasion)
+  siteocc2 <- paste0(site_data$site, site_data$occasion)  
+  if (!all(siteocc %in% siteocc2)) stop("visit_data has sites with occasions not included in site_data")
+  
+  # check start 
+  if (!is.null(start)) {
+    if (!("beta_p" %in% names(start))) stop("start must have list entry named 'beta_p'")
+    if (!("beta_psi" %in% names(start))) stop("start must have a list entry named 'beta_psi'")
+    if (!is.numeric(start$beta_p) | !is.numeric(start$beta_psi)) stop("start vectors are invalid")
+  }
+  
+  if (!is.logical(print)) stop("print must be TRUE or FALSE")
+  
+}
+
+#' Summary of occuR object 
 #'
 #' @param obj output from fit_occu
 #'
-#' @return prints a summary for fixed effect parameters
+#' @return prints a summary
 #' @export
-summary.occu <- function(obj) {
-  return(print(obj$res))
+summary.occuR <- function(obj, conf.level = 0.95) {
+  est <- obj$res$par.fixed 
+  nms <- names(est)
+  sd <- sqrt(diag(obj$res$cov.fixed))
+  alpha <- 1 - (1 - conf.level) / 2
+  lcl <- est - qnorm(alpha) * sd
+  ucl <- est + qnorm(alpha) * sd
+  val <- data.frame(estimate = est, sd = sd, lcl = lcl, ucl = ucl)
+  val <- signif(val, 4)
+  nms[nms == "beta_psi"] <- paste("psi:", colnames(obj$mats$X_psi[,1:obj$mats$nfix_psi, drop=FALSE]))
+  nms[nms == "beta_p"] <- paste("p:", colnames(obj$mats$X_p[,1:obj$mats$nfix_p, drop = FALSE]))
+  rownames(val) <- nms
+  print(val)
+  dof <- round(as.numeric(dof.occuR(obj, each = TRUE)), 1)
+  cat(paste0("Total edf: ", sum(dof), "\t smooth_psi: ", dof[2], "\t smooth_p: ", dof[3]))
+  invisible(obj)
+}
+
+#' Print of occuR object 
+#'
+#' @param obj output from fit_occu
+#'
+#' @return prints a summary 
+#' @export
+print.occuR <- function(obj) {
+  summary(obj)
 }
 
 #' Get predicted values for psi and p from parameters and matrices
@@ -234,7 +309,7 @@ get_predicted_values <- function(fix, ran, mats) {
 #' contains matrix for psi and p where each row is a bootstrap resample
 #' @export
 #' @importFrom mgcv rmvn
-predict.occu <- function(obj, visit_data, site_data, nboot = 0) {
+predict.occuR <- function(obj, visit_data, site_data, nboot = 0) {
   mats <- make_matrices(obj$forms, visit_data, site_data)
   fix <- obj$res$par.fixed
   ran <- obj$res$par.random
@@ -273,7 +348,7 @@ predict.occu <- function(obj, visit_data, site_data, nboot = 0) {
 #' and is based on Wood et al. (2017) p 212. 
 #'
 #' @return estimated degrees of freedom 
-edf.occu <- function(X, S, lambda, Sn){
+edf.occuR <- function(X, S, lambda, Sn){
   if (length(lambda) == 0) return(0)
 
   # duplicate lambda enough times
@@ -294,18 +369,18 @@ edf.occu <- function(X, S, lambda, Sn){
 #' Compute (estimated) degrees of freedom for fitted occupancy model
 #'
 #' @param obj fitted model object 
-#' @param by default is FALSE, if TRUE then list of degrees of freedom is returned, one fixed parameters, smooth
+#' @param each default is FALSE, if TRUE then list of degrees of freedom is returned, one fixed parameters, smooth
 #' for psi, and smooth for p. When FALSE, these are summed and total estimated degrees of freedom returned. 
 #'
-#' @return degrees of freedom (see "by" above)
+#' @return degrees of freedom (see "each" above)
 #' @export
-dof.occu <- function(obj, each = FALSE) {
+dof.occuR <- function(obj, each = FALSE) {
   fix <- obj$res$par.fixed
   df <- length(fix[!grepl("log_lambda", names(fix))])
   psi_lambda <- exp(fix[names(fix) == "log_lambda_psi"])
-  psi_edf <- edf.occu(obj$mats$X_psi[, -(1:obj$mats$nfix_psi)], obj$mats$S_psi$S, psi_lambda, obj$mats$S_psi$Scols)
+  psi_edf <- edf.occuR(obj$mats$X_psi[, -(1:obj$mats$nfix_psi)], obj$mats$S_psi$S, psi_lambda, obj$mats$S_psi$Scols)
   p_lambda <- exp(fix[names(fix) == "log_lambda_p"])
-  p_edf <- edf.occu(obj$mats$X_p[, -(1:obj$mats$nfix_p)], obj$mats$S_p$S, p_lambda, obj$mats$S_p$Scols)
+  p_edf <- edf.occuR(obj$mats$X_p[, -(1:obj$mats$nfix_p)], obj$mats$S_p$S, p_lambda, obj$mats$S_p$Scols)
   if (each) return(list(fix = df, psi = psi_edf, p = p_edf))
   return(df + psi_edf + p_edf)
 }
@@ -317,7 +392,7 @@ dof.occu <- function(obj, each = FALSE) {
 #'
 #' @return log-likelihood with estimated degrees of freedom as attribute "df"
 #' @export
-logLik.occu <- function(object, ...) {
+logLik.occuR <- function(object, ...) {
   val <- -object$fit$objective 
   attributes(val)$df <- dof.occu(object)
   return(val)
